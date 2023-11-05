6a88a704a8563796720c8cae47c9c89d286b652b
Alain Elemia <alain.elemia@gmail.com>
2023-11-05 14:58:54 +0800

    Add initial changes for Round1 (#1)
    Co-authored-by: Alain Elemia <Alain.Elemia@towerswatson.com>

--- src/Brainy.UI/Program.cs
+++ src/Brainy.UI/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.SessionStorage;
 using Brainy.UI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredSessionStorage();
 
 await builder.Build().RunAsync();
---
+++ src/Brainy.UI/State/StateData.cs
@@ -0,0 +1,29 @@
+﻿using Brainy.UI.Enums;
+
+namespace Brainy.UI.State
+{
+	public class StateData
+	{
+		private ICollection<Categories> _categories;
+		public ICollection<Categories> Categories { get => _categories; set { _categories = value; NotifyStateChanged(); } }
+
+		private int _round;
+		public int Round { get => _round; set { _round = value; NotifyStateChanged(); } }
+
+		public void RemoveCategory(Categories category)
+		{
+			Categories.Remove(category);
+
+			NotifyStateChanged();
+		}
+
+		public void Reset()
+		{
+			Categories = Enum.GetValues(typeof(Categories)).Cast<Categories>().ToHashSet();
+			Round = 0;
+		}
+
+		public Action? OnStateChanged { get; set; }
+		private void NotifyStateChanged() => OnStateChanged?.Invoke();
+	}
+}
