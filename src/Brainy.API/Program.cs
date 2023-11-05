2321ee242b8584b770f53079cb2ceab560b699dd
Alain Elemia <Alain.Elemia@towerswatson.com>
2023-11-05 16:17:00 +0800

    Add api project with fake data

---
+++ src/Brainy.API/Program.cs
@@ -0,0 +1,38 @@
+using Brainy.API;
+using Brainy.Contracts.Enums;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(builder =>
+	{
+		builder.AllowAnyOrigin()
+			.AllowAnyHeader()
+			.AllowAnyMethod();
+	});
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(x => x.AllowAnyHeader()
+	.AllowAnyOrigin()
+	.AllowAnyMethod());
+
+app.MapGet("/api/answerSet/{category}", (Category category) => FakeData.GetAnswerSet(category));
+
+app.Run();
